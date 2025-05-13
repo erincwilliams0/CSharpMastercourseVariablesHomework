3c69f88eac5210e290a5f48882cf606ecdfee534
erin williams <erin.cwilliams0@gmail.com>
2025-05-12 21:57:54 -0500

    Add project files.

---
+++ VariablesHomework/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace VariablesHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // opening/welcoming
+            Console.WriteLine("Welcome, this console app simulates creating a user profile");
+            Console.WriteLine("\nWe're going to start off with a couple of questions First");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ReadKey();
+
+            // each portion will...
+            // 1. prompt a user for some input with a consolewriteline
+            // 2. validate the users input
+                // a. if valiidation fails give user more information on why
+                // b. on success store the data belonging to a user class
+
+            //name
+            // string variable name
+            // shouldn't contain numbers or non alphabet characters
+            
+            
+
+
+            //age
+            // int variable age
+            // only acceptable values are 1 through 110
+
+            //if they are alive
+            // simple Boolean store
+
+            //and their phone Number
+            // string variable 
+            // should only contain numbers, dashes, and parenthesis 
+            // should be (123) 123-1234
+            // will store phone number already formatted, or will format the number from user input
+        }
+    }
+}
